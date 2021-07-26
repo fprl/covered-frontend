01c3fe68cd43714e0ac626817941231d84a4387d
francoromanolosada <francoromano1994@gmail.com>
2021-07-26 17:58:36 +0200

    Added propTypes + media queries

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,52 +1,98 @@
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import Logo from './logo/Logo'
-import PhoneIcon from './PhoneIcon'
+import PhoneIcon from './icons/PhoneIcon'
 import Avatar from '../avatar/Avatar'
 
 import { Flex } from '../styled/lib'
 
 const Navbar = ({ initials }) => {
   return (
-    <Nav>
-      <Flex grow={1}>
-        <Link href="#">
-          <Logo />
-        </Link>
-      </Flex>
-      <Divider gap={1}>
-        <PhoneIcon />
-        <Avatar initials={initials} />
-      </Divider>
-    </Nav>
+    <Header>
+      <Nav>
+        <Flex grow={1}>
+          <Link href="#">
+            <Logo />
+          </Link>
+        </Flex>
+        <Divider gap={1}>
+          <Link
+            href="https://www.website.covered.ibizadev.nl/huis-ibiza/?"
+            desktop
+          >
+            Our villas
+          </Link>
+          <Button>
+            <PhoneIcon />
+          </Button>
+          <Avatar initials={initials} />
+        </Divider>
+      </Nav>
+    </Header>
   )
 }
 
 export default Navbar
+
+Navbar.propTypes = {
+  initials: PropTypes.string,
+}
+
+const Header = styled.header`
+  position: absolute;
+  top: 0rem;
+  z-index: 10;
+
+  width: 100%;
+  height: 62px;
+
+  border-bottom: 1px solid var(--color-border-light);
+  margin: auto;
+
+  @media (min-width: 1024px) {
+    height: 80px;
+  }
+`
 
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
 
-  position: absolute;
-  top: 0rem;
-  z-index: 10;
-
-  height: 62px;
-  width: 100%;
   padding: 1rem;
-
-  border-bottom: 1px solid var(--color-border-light);
 
   @media (min-width: 1024px) {
     padding-inline: 5rem;
+    padding-block: 0;
+    height: 80px;
+  }
+
+  @media (min-width: 1600px) {
+    max-width: 1600px;
+    margin: auto;
   }
 `
 
 const Divider = styled(Flex)`
   display: flex;
-  align-items: center;
+  align-items: flex-end;
 `
 
-const Link = styled.a``
+const Link = styled.a`
+  display: ${p => (p.desktop ? 'none' : 'block')};
+
+  font-size: var(--text-base);
+  margin-right: var(--spacing-base);
+
+  @media (min-width: 1024px) {
+    display: ${p => (p.desktop ? 'block' : '')};
+  }
+`
+
+const Button = styled.button`
+  padding: 0;
+
+  background-color: transparent;
+  border: transparent;
+`
